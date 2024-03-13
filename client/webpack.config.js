d4f80d8976709ca353047b73bcc271489cb4e647
RLCastaldy <rlcastaldy@gmail.com>
2024-03-13 15:54:15 -0500

    added workbox plugins and functionality for InjectManifest/GenerateSW

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -18,12 +15,37 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new GenerateSW(),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+      new WebpackPwaManifest({
+
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src/service-worker.js',
+            swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
