c8ea73b72fd8abc4a978cbd8a7aee95dadd7ee44
RLCastaldy <rlcastaldy@gmail.com>
2024-03-13 16:15:31 -0500

    attempting to get build to work

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,8 +7,8 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: '../src/js/index.js',
+      install: '../src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
