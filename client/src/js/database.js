31842e3593a78331759a3743515b3ee78fa74470
RLCastaldy <rlcastaldy@gmail.com>
2024-03-18 15:58:43 -0500

    finished adding methods for get and put

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,11 +14,23 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  console.log('PUT to the database');
+  const db = await openDB ('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, text: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  console.log(allContent);
+  return allContent;
 };
+
 initdb();
